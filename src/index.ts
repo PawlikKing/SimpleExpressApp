67e4675463bb913dc3a2cac5a8b130264c9a908c
PawlikKing <pawiklord1@gmail.com>
2025-10-11 10:58:36 +0200

    Implemented empty app startup porojects

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import path from "path";
 
 dotenv.config();
 
+const PORT = process.env.PORT || '3000';
+
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+
+// REPLACE WITH ROUTERS LATER
+app.use('/main', (req: Request, res: Response) => {
+    res.render('main', {
+        testValue: "HelloWorld"
+    });
+});
+
+app.use("/product", (req: Request, res: Response) => {});
+
+app.use("/*", (req: Request, res: Response) => {
+    res.redirect("/main");
+})
+
+app.listen(PORT, () => {
+  console.log(`App started on port: ${PORT}`);
+})
