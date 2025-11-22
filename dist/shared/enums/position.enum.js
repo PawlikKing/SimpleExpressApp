00cd49639339d82981a7acef2b407ad6039a45ac
PawlikKing <pawiklord1@gmail.com>
2025-11-22 12:43:50 +0100

    Removed dist dir

--- dist/shared/enums/position.enum.js
+++
@@ -1,6 +0,0 @@
-export var Position;
-(function (Position) {
-    Position[Position["SALESMAN"] = 0] = "SALESMAN";
-    Position[Position["MANAGER"] = 1] = "MANAGER";
-    Position[Position["OWNER"] = 2] = "OWNER";
-})(Position || (Position = {}));
--- dist/shared/flags/permission.flags.js
+++
@@ -1,9 +0,0 @@
-export var Permissions;
-(function (Permissions) {
-    Permissions[Permissions["None"] = 0] = "None";
-    Permissions[Permissions["VIEW_DETAILS_PRODUCTS"] = 1] = "VIEW_DETAILS_PRODUCTS";
-    Permissions[Permissions["VIEW_SALES"] = 2] = "VIEW_SALES";
-    Permissions[Permissions["EDIT_PRODUCTS"] = 4] = "EDIT_PRODUCTS";
-    Permissions[Permissions["MANAGE_CLIENTS"] = 8] = "MANAGE_CLIENTS";
-    Permissions[Permissions["MANAGE_EMPLOYEES"] = 15] = "MANAGE_EMPLOYEES"; // 1111
-})(Permissions || (Permissions = {}));
